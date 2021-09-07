e186de0e6c56fdbc3ca52cb6ce10b39f82bed778
Andrew Medeiros <andrew.medeiros00@gmail.com>
2021-09-07 15:28:39 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/programvendas/ProgramvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.programvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/programvendas/ProgramvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.programvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
