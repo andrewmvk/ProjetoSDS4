c313b5110973c50fb5c95e818f9af3283d7baa54
Andrew Medeiros <andrew.medeiros00@gmail.com>
2021-09-12 16:08:45 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum = {
-    sellerName: string,
-    sum: number
-}//Estamos aqui definindo um tipo que irá coletar os dados do nome do vendedor e da soma de suas vendass
+    sellerName: string;
+    sum: number;
+}//Estamos aqui definindo um tipo que irá coletar os dados do nome do vendedor e da soma de suas vendas
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+}
